d696e23b5e4a27db099ec93db95df26fa53e90d0
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 07:53:53 +0530

    13- check size of image

--- lesson2.py
+++ lesson2.py
@@ -3,6 +3,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Load and display an image
-img = cv2.imread('img/dolphin.png',0)
-cv2.imshow('dolphin',img)
+img = cv2.imread('img/dolphin.png', 0)  # 0 for grayscale
+cv2.imshow('dolphin', img)
 cv2.waitKey(0)
+
+# Image Size
+print(img.shape)
