c4131a108d8fc8f2539e720f5d8b75a530eac3ee
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 08:32:58 +0530

    22- color planes plot

--- lesson2.py
+++ lesson2.py
@@ -9,3 +9,15 @@ cv2.waitKey(0)
 
 # size of the image
 print(img.shape)
+
+# all the rows and columns but only 1 plane
+img_red = img[:, :, 1]
+cv2.imshow('red', img_red)
+cv2.waitKey(0)
+
+# size of color channel
+print(img_red.shape)
+px = img_red[150, :]
+print(px)
+plt.plot(px)
+plt.show()
