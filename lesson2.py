c5745365e3a9a506b928922129454e8957f84899
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 08:16:59 +0530

    21- color planes

--- lesson2.py
+++ lesson2.py
@@ -2,15 +2,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Crop an image
-img = cv2.imread('img/bicycle.png', 0)  # 0 for grayscale
-cv2.imshow('bicycle', img)
+# Color image
+img = cv2.imread('img/fruit.png')
+cv2.imshow('fruit', img)
 cv2.waitKey(0)
 
-# Check Image Size
-print(img.shape)  # when cropping limits should be within this range
-
-cropped = img[109:310, 9:160]  # using different range as compared to octave here end rows and columns are not included
-cv2.imshow('cropped', cropped)
-cv2.waitKey(0)
-print(cropped.shape)
+# size of the image
+print(img.shape)
