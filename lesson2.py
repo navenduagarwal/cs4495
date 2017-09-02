946fc9f8284817cf5d6bff5b89c1aaa141d9bc0f
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 08:01:56 +0530

    15 - Inspect image values

--- lesson2.py
+++ lesson2.py
@@ -4,11 +4,19 @@ from matplotlib import pyplot as plt
 
 # Load and display an image
 img = cv2.imread('img/dolphin.png', 0)  # 0 for grayscale
-cv2.imshow('dolphin', img)
-cv2.waitKey(0)
+# cv2.imshow('dolphin', img)
+# cv2.waitKey(0)
+#
+# # Image Size
+# print(img.shape)
+#
+# # Class of Image
+# print(img.dtype)
 
-# Image Size
-print(img.shape)
+# value at given location
+print(img[50, 100])
 
-# Class of Image
-print(img.dtype)
+# value for entire row
+print(img[50, :])
+plt.plot(img[50, :])
+plt.show()
