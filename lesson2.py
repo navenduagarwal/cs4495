e26fedfa7d7f5bfb3c53e73aae0d250251a35456
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 08:12:44 +0530

    19,20- size of cropped image

--- lesson2.py
+++ lesson2.py
@@ -10,7 +10,7 @@ cv2.waitKey(0)
 # Check Image Size
 print(img.shape)  # when cropping limits should be within this range
 
-cropped = img[109:310, 9:160]
+cropped = img[109:310, 9:160]  # using different range as compared to octave here end rows and columns are not included
 cv2.imshow('cropped', cropped)
 cv2.waitKey(0)
 print(cropped.shape)
