c84e483a76d0b2597158a71cbe1b4b51e78d1a6d
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-02 08:10:43 +0530

    18- crop an image

--- lesson2.py
+++ lesson2.py
@@ -2,17 +2,15 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load and display an image
-img = cv2.imread('img/dolphin.png', 0)  # 0 for grayscale
-# cv2.imshow('dolphin', img)
-# cv2.waitKey(0)
-#
-# # Image Size
-# print(img.shape)
-#
-# # Class of Image
-# print(img.dtype)
+# Crop an image
+img = cv2.imread('img/bicycle.png', 0)  # 0 for grayscale
+cv2.imshow('bicycle', img)
+cv2.waitKey(0)
 
-# a slice of image
-slice = img[100:103, 200:203]  # note in open cv we have to change a range a little bit to include boundaries
-print(slice)
+# Check Image Size
+print(img.shape)  # when cropping limits should be within this range
+
+cropped = img[109:310, 9:160]
+cv2.imshow('cropped', cropped)
+cv2.waitKey(0)
+print(cropped.shape)
