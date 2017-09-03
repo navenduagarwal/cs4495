e7c21bf71804092af03bbe95fb10401ec5324d7c
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-03 07:29:11 +0530

    28 - difference between images

--- lesson2.py
+++ lesson2.py
@@ -2,17 +2,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Adding Gaussian noise
+# Image Difference
 
 # load image 1
 dolphin = cv2.imread('img/dolphin.png', 0)
 cv2.imshow('dolphin', dolphin)
 cv2.waitKey(0)
 
-im = np.empty(dolphin.shape, np.uint8)  # creating empty image
-cv2.randu(im, 0, 90)
-cv2.imshow('noise', im)
-cv2.imshow(' image witj noise', dolphin + im)
+# load image 2
+bicycle = cv2.imread('img/bicycle.png', 0)
+cv2.imshow('bicycle', bicycle)
+cv2.waitKey(0)
+
+# difference
+diff = dolphin - bicycle
+cv2.imshow('diff', diff)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()  # close all openCV windows
