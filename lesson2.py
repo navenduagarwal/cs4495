744a6dc3a7f2791f1d388df5b865767191e7b276
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-03 07:26:41 +0530

    27 - adding gaussian noise

--- lesson2.py
+++ lesson2.py
@@ -2,41 +2,17 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Adding two images
+# Adding Gaussian noise
 
 # load image 1
 dolphin = cv2.imread('img/dolphin.png', 0)
 cv2.imshow('dolphin', dolphin)
 cv2.waitKey(0)
 
-# load image 2
-bicycle = cv2.imread('img/bicycle.png', 0)
-cv2.imshow('bicyle', bicycle)
-cv2.waitKey(0)
-
-# check image size is equal
-print('{} image size: {}'.format('Dolphin', dolphin.shape))
-print('{} image size: {}'.format('Bicycle', bicycle.shape))
-
-summed = dolphin + bicycle
-average = dolphin / 2 + bicycle / 2  # Dividing values by 2 to make maximum intensity values same as source values.
-average_alt = (dolphin + bicycle) / 2
-cv_summed = cv2.add(dolphin, bicycle)
-cv_average = cv2.addWeighted(dolphin, 0.5, bicycle, 0.5, 0)
-
-cv2.imshow('summed', summed)
-cv2.waitKey(0)
-
-cv2.imshow('average', average)
-cv2.waitKey(0)
-
-cv2.imshow('average_alt', average_alt)
-cv2.waitKey(0)
-
-cv2.imshow('cv add', cv_summed)
-cv2.waitKey(0)
-
-cv2.imshow('cv add weighed', cv_average)
+im = np.empty(dolphin.shape, np.uint8)  # creating empty image
+cv2.randu(im, 0, 90)
+cv2.imshow('noise', im)
+cv2.imshow(' image witj noise', dolphin + im)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()  # close all openCV windows
