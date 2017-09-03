11908793fa2fd004737f4f1c9361e2da7d744a3d
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-09-03 08:01:29 +0530

    31- converted to gaussian noise

--- lesson2.py
+++ lesson2.py
@@ -9,19 +9,10 @@ dolphin = cv2.imread('img/dolphin.png', 0)
 cv2.imshow('dolphin', dolphin)
 cv2.waitKey(0)
 
-# load image 2
-bicycle = cv2.imread('img/bicycle.png', 0)
-cv2.imshow('bicycle', bicycle)
-cv2.waitKey(0)
-
-# difference
-diff = dolphin - bicycle
-cv2.imshow('diff', diff)
-cv2.waitKey(0)
-
-# difference with cv
-diff_cv = cv2.subtract(dolphin, bicycle)
-cv2.imshow('diff_cv', diff_cv)
+im = np.empty(dolphin.shape, np.uint8)  # creating empty image
+cv2.randn(im, 0, 50) # converted to gaussian noise
+cv2.imshow('noise', im)
+cv2.imshow(' image witj noise', dolphin + im)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()  # close all openCV windows
